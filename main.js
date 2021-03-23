b23b665a9d3fbb2f39143086fc37aec13c05ff0a
Google Project <devin.pfromm.20@gmail.com>
2021-03-22 21:25:35 -0400

    First bit of JS
    Trying to get a central, and much smaller UI that shows what the computer has chosen.

--- main.js
+++ main.js
@@ -7,6 +7,41 @@
     prompt to play again
     - (BONUS) There should be a way to track the progress of 
     each game played in a session
+
+    - need to structure which items win/lose to which
+    - need to set up me, vs computer
+
+    I think if I created two variables. One that kicks
+    off the second. I would select the button, and 
+    that would call the function to randomly select
+    a button for the computer. The two would be weighed,
+    and the results print to the screen.
 */
 
 
+// Rock, Paper, Scissor Button Identifiers
+const rock = document.getElementsByClassName("rock");
+const paper = document.getElementsByClassName("paper");
+const scissors = document.getElementsByClassName("scissors");
+
+// Array for computer randomized selection
+const gameArray = [
+    rock,
+    paper,
+    scissors
+]
+
+// function that gets activated after user's selection
+function computerSelection() {
+    var computer = gameArray[Math.floor(Math.random() * gameArray.length)];
+    buttonClick();
+    return console.log(computer);
+}
+
+// Just a test to see if I can get JS to show that the computer has made a choice
+function buttonClick() {
+    var sciButton = document.getElementsByClassName("scissors");
+    
+}
+
+computerSelection();
