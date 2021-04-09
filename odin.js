1a408e38f38731b48bb9efec2d5945adf303ef10
Google Project <devin.pfromm.20@gmail.com>
2021-04-09 10:22:38 -0400

    Added scoring to the app

--- odin.js
+++ odin.js
@@ -1,6 +1,6 @@
-let rock = "rock"; //= document.getElementsByClassName("rock");
-let paper = "paper"; //= document.getElementsByClassName("paper");
-let scissors= "scissors"; //= document.getElementsByClassName("scissors");
+let rock = "Rock"; //= document.getElementsByClassName("rock");
+let paper = "Paper"; //= document.getElementsByClassName("paper");
+let scissors= "Scissors"; //= document.getElementsByClassName("scissors");
 
 // game information
 let playerSelection;
@@ -13,9 +13,25 @@ let rButton = document.querySelector(".rock");
 let pButton = document.querySelector(".paper");
 let sButton = document.querySelector(".scissors");
 
+// selectors for indiv. comp buttons
+let compRButton = document.querySelector('.comrock');
+let compPButton = document.querySelector('.compaper');
+let compSButton = document.querySelector('.comscissors');
+
+// values for highlighting comp choice
+compRButton.setAttribute('class', 'compRButtonDefault'); 
+compPButton.setAttribute('class', 'compPButtonDefault'); 
+compSButton.setAttribute('class', 'compSButtonDefault'); 
+
 // for printing results to screen
 
 const results = document.querySelector('.results');
+const yourResults = document.querySelector('.yourResults');
+const compResults = document.querySelector('.compResults');
+yourResults.innerHTML += `<h3> Your Score </h3> ${playerScore}`;
+compResults.innerHTML = `<h3> Computer </h3> ${computerScore}`
+                    
+
 const para = document.createElement('p');
 
 // gameArray and compChoice needed for computer's random selection
@@ -25,9 +41,15 @@ const gameArray = [
     scissors
 ]
 
+
+
+
+
 function computersChoice() {
     var computer = gameArray[Math.floor(Math.random()
             * gameArray.length)];
+       
+
     return computer;
 }
 
@@ -35,7 +57,29 @@ let computerSelection = computersChoice();
 
 
 
+function visibleCompChoice(computer) {
+    
+        
 
+        switch (true) {
+            case computer === rock:
+                compRButton.setAttribute('class', 'compRButton'); 
+                compPButton.setAttribute('class', 'compPButtonDefault'); 
+                compSButton.setAttribute('class', 'compSButtonDefault');
+            break;
+            case computer === paper:
+                compPButton.setAttribute('class', 'compPButton'); 
+                compRButton.setAttribute('class', 'compPButtonDefault'); 
+                compSButton.setAttribute('class', 'compSButtonDefault');
+            break;
+            case computer === scissors:
+                compSButton.setAttribute('class', 'compSButton'); 
+                compPButton.setAttribute('class', 'compPButtonDefault'); 
+                compRButton.setAttribute('class', 'compSButtonDefault');
+            break;
+        }
+
+}
 
 
 
@@ -53,13 +97,17 @@ function gamePlay(playerSelection, computerSelection) {
         case playerSelection === scissors && computerSelection === paper:
             para.textContent =`You win! ${playerSelection} beats ${computerSelection}!`;
             playerScore += 1;
+            yourResults.innerHTML = `<h3> Your Score </h3> ${playerScore}`;
+            
             break;
             
         default:
             para.textContent =`${computerSelection} beats ${playerSelection} You lose.`;
             computerScore += 1;
+            compResults.innerHTML = `<h3> Computer </h3> ${computerScore}`;
     }
    
+    
 }
 
 function scoreTracker() {
@@ -91,21 +139,22 @@ function rounds() {
 
 function score() {
     if (round === 5 && playerScore > computerScore) {
-        const playerSco = document.createElement('h4');
+        const playerSco = document.createElement('h3');
         playerSco.textContent = `${playerScore} to ${computerScore}, you won!`;
         console.log(playerSco);
         results.appendChild(playerSco);
         results.style.cssText = "text-align: center; background-color: #b6fdfa; border-radius: 5px; box-shadow: 2px 2px 2px 2px  rgba(52, 125, 121, .6);";
         
     } else {
-        const compSco = document.createElement('h4');
+        const compSco = document.createElement('h3');
         compSco.textContent = `${computerScore} to ${playerScore}, you lost..`;
         console.log(compSco);
         results.appendChild(compSco);
-        results.style.cssText = "text-align: center; background-color: #b6fdfa; border-radius: 5px; box-shadow: 2px 2px 2px 2px; box-shadow: 2px 2px 2px 2px  rgba(52, 125, 121, .6);";
+        results.style.cssText = "text-align: center; background-color: #b6fdfa; border-radius: 5px; box-shadow: 2px 2px 2px 2px  rgba(52, 125, 121, .6);";
         
         
     }
+
 }
 
 // -------------------------------------------- Event listeners for buttons 
@@ -115,6 +164,8 @@ rButton.addEventListener('click', () => {
     let computerSelection = computersChoice();
     round += 1;
 
+
+    visibleCompChoice(computerSelection)
     gamePlay(playerSelection, computerSelection);
     console.log(playerScore, computerScore);
     results.appendChild(para);
@@ -128,6 +179,8 @@ rButton.addEventListener('click', () => {
     } else {
        console.log(round);
     }
+
+
 })
 
 pButton.onclick = function() {
@@ -135,6 +188,8 @@ pButton.onclick = function() {
     let computerSelection = computersChoice();
     round += 1;
 
+    
+    visibleCompChoice(computerSelection)
     gamePlay(paper, computerSelection);
 
     console.log(playerScore, computerScore);
@@ -157,6 +212,7 @@ sButton.onclick = function() {
     let computerSelection = computersChoice();
     round += 1;
 
+    visibleCompChoice(computerSelection)
     gamePlay(scissors, computerSelection);
     console.log(playerScore, computerScore);
     results.appendChild(para);
@@ -174,7 +230,6 @@ sButton.onclick = function() {
    
         
 }
-
 
 
 
