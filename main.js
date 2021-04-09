4ac3304359bf19afae613d3ba38b43c2eabd2463
Google Project <devin.pfromm.20@gmail.com>
2021-04-09 07:23:20 -0400

    Added proper game logic

--- main.js
+++ main.js
@@ -63,3 +63,99 @@ function priorityLevel() {
 }
 
 */
+let rock = "rock"; //= document.getElementsByClassName("rock");
+let paper = "paper"; //= document.getElementsByClassName("paper");
+let scissors= "scissors"; //= document.getElementsByClassName("scissors");
+let playerSelection;
+let playerScore = 0;
+let computerScore = 0;
+
+const gameArray = [
+    rock,
+    paper,
+    scissors
+]
+
+function roundCounter() {
+    round += 1;
+    return round;
+}
+    
+   
+function computersChoice() {
+    var computer = gameArray[Math.floor(Math.random()
+            * gameArray.length)];
+    return computer;
+}
+
+let computerSelection = computersChoice();
+
+
+
+/*
+function game() {
+    gameArray.addEventListener("click", gamePlay(playerSelection, computerSelection));
+    
+    }
+*/
+/* Copied all of the below text from the odin.js page. 
+got to the point that they wanted me to input different code 
+in the odin.js file for the completion of this project instead of this code */
+function gamePlay(playerSelection, computerSelection) {
+    switch (true) {
+        case playerSelection === computerSelection:
+            console.log(`Its a tie! You both chose ${playerSelection}! Try again!`);
+            break;
+        case playerSelection === rock && computerSelection === scissors:
+        case playerSelection === paper && computerSelection === rock:
+        case playerSelection === scissors && computerSelection === paper:
+            console.log(`${playerSelection} beats ${computerSelection}!`);
+            playerScore += 1;
+            
+            break;
+        default:
+            console.log(`${computerSelection} beats ${playerSelection} You lose. Try again!`);
+            computerScore += 1;
+    }
+   
+}
+
+function scoreTracker() {
+    switch (true) {
+        case (playerScore > computerScore):
+            console.log(`You scored ${playerScore}, and the computer ${computerScore}! You win!`);
+            break;
+        default: 
+            console.log(`The computer scored ${computerScore}, you scored ${playerScore}! You lost..`)
+    }
+}
+
+function game() {
+    for (round = 1; round < 6; round++) {
+        console.log(`Round ${round}`);
+        gamePlay(rock, computerSelection);
+        
+    }
+    console.log("----------------- Final Score ---------------------");
+    scoreTracker();
+}
+game();
+
+
+let rButton = document.querySelector(".rock");
+let pButton = document.querySelector(".paper");
+let sButton = document.querySelector(".scissors");
+
+rButton.onclick = function() {
+    gamePlay(rock, computerSelection);
+
+    
+}
+
+pButton.onclick = function() {
+    gamePlay(paper, computerSelection);
+}
+
+sButton.onclick = function() {
+    gamePlay(scissors, computerSelection);
+}
--- odin.js
+++ odin.js
@@ -1,22 +1,30 @@
 let rock = "rock"; //= document.getElementsByClassName("rock");
 let paper = "paper"; //= document.getElementsByClassName("paper");
 let scissors= "scissors"; //= document.getElementsByClassName("scissors");
+
+// game information
 let playerSelection;
 let playerScore = 0;
 let computerScore = 0;
+let round = 0; 
 
+// selectors for indiv. buttons
+let rButton = document.querySelector(".rock");
+let pButton = document.querySelector(".paper");
+let sButton = document.querySelector(".scissors");
+
+// for printing results to screen
+
+const results = document.querySelector('.results');
+const para = document.createElement('p');
+
+// gameArray and compChoice needed for computer's random selection
 const gameArray = [
     rock,
     paper,
     scissors
 ]
 
-function roundCounter() {
-    round += 1;
-    return round;
-}
-    
-   
 function computersChoice() {
     var computer = gameArray[Math.floor(Math.random()
             * gameArray.length)];
@@ -27,27 +35,28 @@ let computerSelection = computersChoice();
 
 
 
-/*
-function game() {
-    gameArray.addEventListener("click", gamePlay(playerSelection, computerSelection));
-    
-    }
-*/
+
+
+
+
+
+// ---------------------------- game logic
 
 function gamePlay(playerSelection, computerSelection) {
+    
     switch (true) {
-        case playerSelection === computerSelection:
-            console.log(`Its a tie! You both chose ${playerSelection}! Try again!`);
+        case computerSelection === playerSelection:
+            para.textContent =`Its a tie! You both chose ${computerSelection}!`;
             break;
         case playerSelection === rock && computerSelection === scissors:
         case playerSelection === paper && computerSelection === rock:
         case playerSelection === scissors && computerSelection === paper:
-            console.log(`${playerSelection} beats ${computerSelection}!`);
+            para.textContent =`You win! ${playerSelection} beats ${computerSelection}!`;
             playerScore += 1;
-            
             break;
+            
         default:
-            console.log(`${computerSelection} beats ${playerSelection} You lose. Try again!`);
+            para.textContent =`${computerSelection} beats ${playerSelection} You lose.`;
             computerScore += 1;
     }
    
@@ -56,20 +65,123 @@ function gamePlay(playerSelection, computerSelection) {
 function scoreTracker() {
     switch (true) {
         case (playerScore > computerScore):
-            console.log(`You scored ${playerScore}, and the computer ${computerScore}! You win!`);
+            para.textContent =`You scored ${playerScore}, and the computer ${computerScore}! You win!`;
             break;
         default: 
-            console.log(`The computer scored ${computerScore}, you scored ${playerScore}! You lost..`)
+            para.textContent = `The computer scored ${computerScore}, you scored ${playerScore}! You lost..`;
+    }
+
+};
+
+
+function rounds() {
+    const ask = document.createElement('h2');
+    const yes = document.createElement('button');
+    const no = document.createElement('button');
+
+    ask.textContent = 'Play again?';
+    yes.textContent = 'Yes';
+    no.textContent = 'No';
+
+    results.appendChild(ask);
+    results.appendChild(yes);
+    results.appendChild(no);
+    
+}
+
+function score() {
+    if (round === 5 && playerScore > computerScore) {
+        const playerSco = document.createElement('h4');
+        playerSco.textContent = `${playerScore} to ${computerScore}, you won!`;
+        console.log(playerSco);
+        results.appendChild(playerSco);
+        results.style.cssText = "text-align: center; background-color: #b6fdfa; border-radius: 5px; box-shadow: 2px 2px 2px 2px  rgba(52, 125, 121, .6);";
+        
+    } else {
+        const compSco = document.createElement('h4');
+        compSco.textContent = `${computerScore} to ${playerScore}, you lost..`;
+        console.log(compSco);
+        results.appendChild(compSco);
+        results.style.cssText = "text-align: center; background-color: #b6fdfa; border-radius: 5px; box-shadow: 2px 2px 2px 2px; box-shadow: 2px 2px 2px 2px  rgba(52, 125, 121, .6);";
+        
+        
     }
 }
 
-function game() {
-    for (round = 1; round < 6; round++) {
-        console.log(`Round ${round}`);
-        gamePlay(rock, computerSelection);
-        
+// -------------------------------------------- Event listeners for buttons 
+
+rButton.addEventListener('click', () => {
+    let playerSelection = rock;
+    let computerSelection = computersChoice();
+    round += 1;
+
+    gamePlay(playerSelection, computerSelection);
+    console.log(playerScore, computerScore);
+    results.appendChild(para);
+
+
+    if (round === 5) {
+        console.log(`Round ${round} !End of game!`);
+        score();
+        rounds();
+
+    } else {
+       console.log(round);
     }
-    console.log("----------------- Final Score ---------------------");
-    scoreTracker();
+})
+
+pButton.onclick = function() {
+    playersSelection = paper;
+    let computerSelection = computersChoice();
+    round += 1;
+
+    gamePlay(paper, computerSelection);
+
+    console.log(playerScore, computerScore);
+    results.appendChild(para);
+
+    if (round === 5) {
+        console.log(`Round ${round} !End of game!`);
+        score();
+        rounds();
+    } else {
+       console.log(round);
+    }
+  
+
+    
 }
-game();
+
+sButton.onclick = function() {
+    playerSelection = scissors;
+    let computerSelection = computersChoice();
+    round += 1;
+
+    gamePlay(scissors, computerSelection);
+    console.log(playerScore, computerScore);
+    results.appendChild(para);
+
+
+    if (round === 5) {
+        console.log(`Round ${round} !End of game!`);
+        score();
+        rounds();
+        
+
+    } else {
+       console.log(round);
+    }
+   
+        
+}
+
+
+
+
+
+
+
+    
+  
+
+
